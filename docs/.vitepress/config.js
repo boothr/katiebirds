f9fd6974230d43caccd6b727801281987c15c3e8
Ryan Booth <ryan.cs@gmail.com>
2022-01-04 06:12:27 -0600

    deploy script

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,4 +1,31 @@
 module.exports = {
     title: "Katie's Bird Site",
+    description: 'My world of birds.', //Website description
     lang: "en-US",
-}
+    base: '/katiebirds/', //  The default path during deployment / secondary address / base can be used/
+    // Lang: 'en US', / / language
+    // Page header configuration, icon, css, js
+    head: [
+      // Change the icon of the title
+//      [
+    //     'link',
+    //     {
+    //       rel: 'icon',
+    //       href: '/img/linktolink.png',//The pictures are placed in the public folder
+    //     },
+    //   ],
+    ],
+    // Theme configuration
+    themeConfig: {
+      repo: 'vuejs/vitepress', // Your github warehouse address will jump in the upper right corner of the page
+      //   Head navigation
+      nav: [
+        { text: 'Home', link: '/' },
+        { text: 'Birds', link: '/birds/' },
+      ],
+      //   Side navigation
+    //   sidebar: [
+    //     { text: 'my', link: '/mine/' }
+    //   ]
+    }
+  }
